ba3437c246f07c2f56b9c2481ae236db3d8c6435
xuyachang <1031372858@qq.com>
2024-01-28 20:32:28 +0800

    新增MybatisPlusAutoGenerator代码自动生成器

--- xyc-domain-order-implement/src/main/java/org/xyc/domain/order/util/MybatisPlusAutoGeneratorUtil.java
+++ xyc-domain-order-implement/src/main/java/org/xyc/domain/order/util/MybatisPlusAutoGeneratorUtil.java
@@ -15,22 +15,44 @@ public class MybatisPlusAutoGeneratorUtil {
                 "jdbc:postgresql://47.94.163.139:5432/xyc_mall",
                 "postgres",
                 "postgres");
+        //mybatisPlus总共支持6种配置
         FastAutoGenerator.create(config)
+                //自定义全局配置
                 .globalConfig((scanner,builder)  -> {
+                    //配置作者
                     builder.author("xuyachang")
+                            //配置时间
                             .commentDate("yyyy-MM-dd")
+                            //输出目录
                             .outputDir("D:\\workspace\\private\\xyc-domain-order\\xyc-domain-order-implement\\src\\main\\java");
                 })
+                //包配置
                 .packageConfig((scanner,builder) -> {
+                    //配置父包名
                     builder.parent("org.xyc.domain.order");
-                        })
+                })
+                //策略配置
                 .strategyConfig((scanner,builder)  -> {
                     // 设置需要生成的表名
-                    builder.addInclude("order_p_o");
+                    builder.addInclude("order_p_o")
+                            //配置模型
+                            .entityBuilder().enableLombok().enableFileOverride()
+                            //配置控制器
+                            .controllerBuilder().enableRestStyle().enableFileOverride()
+                            //配置服务
+                            .serviceBuilder().enableFileOverride()
+                            //配置Mapper
+                            .mapperBuilder().enableFileOverride();
                 })
+                //模板配置
                 .templateConfig((scanner,builder)->{
-                    builder.disable(TemplateType.CONTROLLER);
+                    //配置不需要生成的文件
+                    builder.disable(TemplateType.CONTROLLER,TemplateType.SERVICE,TemplateType.SERVICE_IMPL);
                 })
+                //注入配置
+//                .injectionConfig((scanner,builder)  ->{})
+                //模板引擎配置
+//                .templateConfig(builder->{})
                 .execute();
     }
 
